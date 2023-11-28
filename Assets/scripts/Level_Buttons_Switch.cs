1d60069b16ed23b52fcd4de88c04b7fb141e480e
Gary <144367122+100030295@users.noreply.github.com>
2023-11-28 09:17:42 -0800

    .

---
+++ Assets/scripts/Level_Buttons_Switch.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;    
+using UnityEngine;
+using System;
+
+public class Level_Buttons_Switch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public button 
+        
+        
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
