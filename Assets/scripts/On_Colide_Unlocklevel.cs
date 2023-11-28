5ea2320b499dd7fc27cf4404fde34e4375efe65e
Gary <144367122+100030295@users.noreply.github.com>
2023-11-28 09:46:08 -0800

    forlater

---
+++ Assets/scripts/On_Colide_Unlocklevel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class On_Colide_Unlocklevel : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        
+        
+    }
+
+
+
+
+
+
+
+
+}
