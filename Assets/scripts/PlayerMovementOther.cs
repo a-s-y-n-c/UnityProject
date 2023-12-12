636e730a760f774bbe370a59a860ca4d62bee319
EthanDangerous2 <144707441+EthanDangerous2@users.noreply.github.com>
2023-12-12 09:41:28 -0800

    .

--- Assets/scripts/PlayerMovementOther.cs
+++ Assets/scripts/PlayerMovementOther.cs
@@ -18,7 +18,7 @@ public class PlayerMovementOther : MonoBehaviour
     private float wallJumpingTime = 0.3f;
     private float wallJumpingCounter;
     private float wallJumpingDuration = 0.1f;
-    [SerializeField] private Vector2 wallJumpingPower = new Vector2(4.5f, 8f);
+    [SerializeField] private Vector2 wallJumpingPower = new Vector2(4.75f, 8f);
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
