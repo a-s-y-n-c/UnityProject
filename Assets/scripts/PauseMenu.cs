7c0284fb52c9eee7fc8e573d0c859f41f90e1fa8
EthanDangerous2 <144707441+EthanDangerous2@users.noreply.github.com>
2023-12-19 09:43:01 -0800

    .

--- Assets/scripts/PauseMenu.cs
+++ Assets/scripts/PauseMenu.cs
@@ -14,7 +14,7 @@ public class PauseMenu : MonoBehaviour
     
     public void Home()
     {
-        SceneManager.LoadScene("Main Menu");
+        SceneManager.LoadScene("Main_menu");
         Time.timeScale = 1;
     }
     
